f0b7a0ddf79fef6a9e4fda1b14f45f6313eb88e0
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-25 20:52:14 +0300

    Добавлен репозиторий

---
+++ ToDoApp/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Repositories
+{
+    public interface IRepository<T> where T : IEntity, new()
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T?> GetByID(int id);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ ToDoApp/Repositories/TaskRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.DbContexts;
+using ToDoApp.Models;
+
+namespace ToDoApp.Repositories
+{
+    public class TaskRepository : IRepository<TaskItem>
+    {
+        ITasksDbContextFactory _dbContextFactory;
+
+        public TaskRepository(ITasksDbContextFactory factory)
+        {
+            _dbContextFactory = factory;
+        }
+
+        public async Task Create(TaskItem entity)
+        {
+            using TasksDbContext context = _dbContextFactory.CreateDbContext();
+            context.Tasks.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(TaskItem entity)
+        {
+            using TasksDbContext context = _dbContextFactory.CreateDbContext();
+            context.Tasks.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TaskItem entity)
+        {
+            using TasksDbContext context = _dbContextFactory.CreateDbContext();
+            context.Tasks.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAll()
+        {
+            using TasksDbContext context = _dbContextFactory.CreateDbContext();
+            return await context.Tasks.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetByID(int id)
+        {
+            using TasksDbContext context = _dbContextFactory.CreateDbContext();
+            return await context.Tasks.FindAsync(id);
+        }
+    }
+}
