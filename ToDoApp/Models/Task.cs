c612338ee04393eb10ca666f2fcdbcbbcc637855
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-25 18:12:19 +0300

    Добавьте файлы проекта.

---
+++ ToDoApp/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Task
+    {
+        public Task()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CompletedDateTime { get; set; }
+    }
+}
