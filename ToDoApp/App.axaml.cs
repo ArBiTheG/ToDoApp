caaa65ef7b34629a2349140b6d267f131b3b1c35
ArBiTheG <56531440+ArBiTheG@users.noreply.github.com>
2025-07-25 20:06:42 +0300

    Добавлен сервис

--- ToDoApp/App.axaml.cs
+++ ToDoApp/App.axaml.cs
@@ -1,7 +1,10 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ToDoApp.DbContexts;
 using ToDoApp.ViewModels;
 using ToDoApp.Views;
 
@@ -9,6 +12,22 @@ namespace ToDoApp;
 
 public partial class App : Application
 {
+    private IHost _host;
+
+    public App()
+    {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainViewModel>();
+
+                string? connectionString = hostContext.Configuration.GetConnectionString("Default");
+                if (connectionString != null )
+                    services.AddSingleton<ITasksDbContextFactory>(new TasksDbContextFactory(connectionString));
+
+            })
+            .Build();
+    }
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -16,18 +35,28 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        _host.Start();
+
+        ITasksDbContextFactory contextFactory = _host.Services.GetRequiredService<ITasksDbContextFactory>();
+        using (TasksDbContext context = contextFactory.CreateDbContext())
+        {
+            context.Database.EnsureCreated();
+        }
+
+        var mainViewModel = _host.Services.GetRequiredService<MainViewModel>();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = mainViewModel
             };
         }
 
